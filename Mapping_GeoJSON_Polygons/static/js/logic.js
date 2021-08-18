50836516a1cdf54618ab7a1c11dac2c2b8ec0635
ireion_a <iranthon@gmail.com>
2021-08-18 12:16:37 -0500

    completed mapping polygons

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -33,23 +33,18 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-//Accessing the airport GeoJSON URL
-//let airportData = "https://raw.githubusercontent.com/Polo-Marie/Mapping_Earthquakes/main/Mapping_GeoJSON_Points/majorAirports.json";
-
-// Accessing the Toronto airline routes GeoJSON URL.
-//let torontoData = "https://raw.githubusercontent.com/Polo-Marie/Mapping_Earthquakes/main/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
-
 // Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+let torontoHoods = "https://raw.githubusercontent.com/Polo-Marie/Mapping_Earthquakes/main/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
 // Create a style for the lines.
 let myStyle = {
-  color: "#ffffa1",
-  weight: 2
+  color: "#0000CD",
+  weight: 1,
+  fillColor: "#ffff00"
 }
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
   console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   //L.geoJson(data).addTo(map);
@@ -60,7 +55,7 @@ d3.json(torontoData).then(function(data) {
     // We turn each feature into a marker on the map.
     onEachFeature: function(feature, layer) {
       //console.log(layer);
-      layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr> <h3> Destination: " + feature.properties.dst + "</h3>");
+      layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
     }
   })
   .addTo(map);
